bcd969f7f9db1640852a4c88cbf7bd0632ae2140
PratikshaC2194 <97968229+PratikshaC2194@users.noreply.github.com>
2022-03-04 18:56:59 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,138 @@
+//Decision Making Statement
+
+//let a =10;
+//if(a==10)
+
+//sucess{
+ //   else{
+        //failure
+  //  }
+//}
+
+//if else ladder
+//let a=-10;
+//if(a<0)
+//{
+//    console.log("Negative");
+//}
+//else if(a==0)
+//{
+  // console.log("Zero");
+//}
+//else{
+   // console.log("Positive");
+//}
+// Tere or more condition
+//Switch case statement
+
+//let gender="a";
+//switch(gender)
+//{
+    //case "a":
+               // console.log("Male");
+               // break;
+  //  case "f":
+                //console.log("Female");
+                //break;
+   // default:
+                //console.log("transgender");
+                //break;
+//}
+
+// Q. Take age as avariable 
+//age<18 = Child
+//greater then 18 but less then 40 =Adult
+//age is greater than 40=Old Age
+//
+//let age=27;
+//switch(true)
+////{
+  //  case age<18:
+    
+//console.log("Child");
+          //  break;
+
+    //case 18<age<40:
+           // console.log("Adult");
+           // break;
+   // default:
+          //   console.log("Old Age");
+           // break;
+//}
+
+//take variable name as day , its possible values are 0 1 2 3 4 5
+// 0 = Monday 
+// 5 : Saturday
+//let day;
+//switch(day)
+//{
+ //   case 0:
+          //  console.log("monday");
+          //  break;
+
+    //case 1:
+           // console.log("Tuesday");
+           // break;
+    //case 2:
+            //console.log("Wednesday");
+            //break;
+    //case 3:
+           // console.log("Thursday");
+           // break;
+    //case 4:
+          //  console.log("Friday");
+              //  break;
+    //case 5:
+//console.log("saturday");
+            //break;
+  //  default :
+           // console.log("Sunday");
+          //  break;
+
+
+//}
+
+//Loops
+//Iteration  Statement
+
+//Loops -For,while,do while, 
+
+//for Loops
+//console.log("1");
+//console.log("2");
+//console.log("3");
+//console.log("3");
+//console.log("4");
+//console.log("5");
+//console.log("6");
+//console.log("7");
+//console.log("8");
+//console.log("9");
+//console.log("10");
+
+//for(let i =0;i<=10;i++)
+//{
+  //  console.log("")
+//}
+
+//var a;//created variable
+
+//declare a variable
+
+//var a=20;
+
+//We are initializing the variable with the value of 12
+
+//Q. Print the table of 19 like 19*1=19
+
+// 19 * 2 = 38
+
+let n=19;
+
+for(let i =1;i<=10;i++)
+{
+    result=i*n;
+    console.log(result);
+}
+
+
